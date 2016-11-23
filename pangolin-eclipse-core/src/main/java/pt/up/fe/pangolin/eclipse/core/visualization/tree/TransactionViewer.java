450d261ce78df245276e4aa6e166ba6fdb7feba9
Alexandre Perez <alexandrecperez@gmail.com>
2016-11-23 15:58:11 -0800

    Minor updates.

--- pangolin-eclipse-core/src/main/java/pt/up/fe/pangolin/eclipse/core/visualization/tree/TransactionViewer.java
+++ pangolin-eclipse-core/src/main/java/pt/up/fe/pangolin/eclipse/core/visualization/tree/TransactionViewer.java
@@ -12,7 +12,7 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
-public class TransactionViewer {
+public class TransactionViewer implements ICheckStateListener {
 
 	private ViewPart viewPart;
 	private CheckboxTreeViewer treeViewer;
@@ -45,16 +45,7 @@ public class TransactionViewer {
 		this.treeViewer.setCheckStateProvider(contentProvider);
 		this.treeViewer.addDoubleClickListener(contentProvider);
 
-		final CheckboxTreeViewer tv = this.treeViewer;
-		this.treeViewer.addCheckStateListener(new ICheckStateListener() {
-
-			@Override
-			public void checkStateChanged(CheckStateChangedEvent event) {
-				TransactionTreeNode element = (TransactionTreeNode) event.getElement();
-				element.setChecked(event.getChecked());
-				tv.refresh();
-			}
-		});
+		this.treeViewer.addCheckStateListener(this);
 	}
 
 	public void setInput(final TransactionTree t) {
@@ -63,7 +54,17 @@ public class TransactionViewer {
 			@Override
 			public void run() {
 				tv.setInput(t);
-				//tv.refresh();
+				tv.expandAll();
+			}
+		});
+	}
+
+	public void refresh() {
+		final CheckboxTreeViewer tv = this.treeViewer;
+		PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+			@Override
+			public void run() {
+				tv.refresh();
 			}
 		});
 	}
@@ -72,6 +73,13 @@ public class TransactionViewer {
 	}
 
 	public void dispose() {
+	}
+
+	@Override
+	public void checkStateChanged(CheckStateChangedEvent event) {
+		TransactionTreeNode element = (TransactionTreeNode) event.getElement();
+		element.setChecked(event.getChecked());
+		this.treeViewer.refresh();
 	}
 
 }
