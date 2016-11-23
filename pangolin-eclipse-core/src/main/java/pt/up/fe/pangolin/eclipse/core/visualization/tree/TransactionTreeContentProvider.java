d2d6bf3723a14b99ace3afb117e52611c7503380
Alexandre Perez <alexandrecperez@gmail.com>
2016-11-23 15:37:30 -0800

    Single tree viewer content provider

--- pangolin-eclipse-core/src/main/java/pt/up/fe/pangolin/eclipse/core/visualization/tree/TransactionTreeContentProvider.java
+++ pangolin-eclipse-core/src/main/java/pt/up/fe/pangolin/eclipse/core/visualization/tree/TransactionTreeContentProvider.java
@@ -1,8 +1,15 @@
 package pt.up.fe.pangolin.eclipse.core.visualization.tree;
 
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.ICheckStateProvider;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.swt.graphics.Image;
 
-public class TransactionTreeContentProvider implements ITreeContentProvider {
+public class TransactionTreeContentProvider implements
+		ITreeContentProvider, ILabelProvider, ICheckStateProvider, IDoubleClickListener {
 
 	@Override
 	public Object[] getElements(Object inputElement) {
@@ -37,4 +44,66 @@ public class TransactionTreeContentProvider implements ITreeContentProvider {
 		return false;
 	}
 
+	@Override
+	public void addListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isLabelProperty(Object element, String property) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Image getImage(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getText(Object element) {
+		if (element instanceof TransactionTreeNode) {
+			return ((TransactionTreeNode) element).getName();
+		}
+		else if (element instanceof String) {
+			return (String) element;
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isGrayed(Object element) {
+		if (element instanceof TransactionTreeNode) {
+			return ((TransactionTreeNode) element).isGrayed();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean isChecked(Object element) {
+		if (element instanceof TransactionTreeNode) {
+			return ((TransactionTreeNode) element).isChecked();
+		}
+		return false;
+	}
+
+	@Override
+	public void doubleClick(DoubleClickEvent event) {
+		System.out.println("double clicked " + event.getSelection());
+
+	}
 }
--- pangolin-eclipse-core/src/main/java/pt/up/fe/pangolin/eclipse/core/visualization/tree/TransactionViewer.java
+++ pangolin-eclipse-core/src/main/java/pt/up/fe/pangolin/eclipse/core/visualization/tree/TransactionViewer.java
@@ -20,7 +20,7 @@ public class TransactionViewer {
 	public static TransactionViewer newTransactionViewer(Composite parent, ViewPart viewPart) {
 		return new TransactionViewer(parent, viewPart);
 	}
-	
+
 	public static void revealView() {
 		PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
 			@Override
@@ -38,37 +38,13 @@ public class TransactionViewer {
 	public TransactionViewer(Composite parent, ViewPart viewPart) {
 		this.viewPart = viewPart;
 		this.treeViewer = new CheckboxTreeViewer(parent, SWT.NONE);
-		this.treeViewer.setContentProvider(new TransactionTreeContentProvider());
-		this.treeViewer.setLabelProvider(new TransactionLabelProvider());
 
+		TransactionTreeContentProvider contentProvider = new TransactionTreeContentProvider();
+		this.treeViewer.setContentProvider(contentProvider);
+		this.treeViewer.setLabelProvider(contentProvider);
+		this.treeViewer.setCheckStateProvider(contentProvider);
+		this.treeViewer.addDoubleClickListener(contentProvider);
 
-		this.treeViewer.addDoubleClickListener(new IDoubleClickListener() {
-			@Override
-			public void doubleClick(DoubleClickEvent event) {
-				System.out.println("double clicked " + event.getSelection());
-
-			}
-		});
-
-		this.treeViewer.setCheckStateProvider(new ICheckStateProvider() {
-
-			@Override
-			public boolean isGrayed(Object element) {
-				if (element instanceof TransactionTreeNode) {
-					return ((TransactionTreeNode) element).isGrayed();
-				}
-				return false;
-			}
-
-			@Override
-			public boolean isChecked(Object element) {
-				if (element instanceof TransactionTreeNode) {
-					return ((TransactionTreeNode) element).isChecked();
-				}
-				return false;
-			}
-		});
-		
 		final CheckboxTreeViewer tv = this.treeViewer;
 		this.treeViewer.addCheckStateListener(new ICheckStateListener() {
 
@@ -78,47 +54,6 @@ public class TransactionViewer {
 				element.setChecked(event.getChecked());
 				tv.refresh();
 			}
-			
-			/*public void checkParents(TransactionTreeNode element) {
-			    if (element == null) {
-			        return;
-			    }
-
-			    if (element != null) {
-			        boolean allChecked = true;
-			        boolean allUnchecked = true;
-			        for (final Object fieldElement : element.getChildren()) {
-			            allChecked = allChecked && tv.getChecked(fieldElement);
-			            allUnchecked = allUnchecked && !tv.getChecked(fieldElement);
-			        }
-			        
-			        if (allUnchecked) {
-			            tv.setChecked(element, false);
-			            tv.setGrayed(element, false);
-			        } else
-			        if (allChecked) {
-			            tv.setChecked(element, true);
-			            tv.setGrayed(element, false);
-			        } else
-			        if (!allUnchecked && !allChecked) {
-			            tv.setChecked(element, true);
-			            tv.setGrayed(element, true);
-			        }
-			    }
-			    checkParents( (TransactionTreeNode) element.getParent());
-			}
-
-			public void checkChildren(TransactionTreeNode element, final boolean checked) {
-			    if (element == null) {
-			        return;
-			    }
-
-			    if (element != null) {
-			        tv.setChecked(element, checked);
-			        tv.setGrayed(element, false);
-			        tv.setSubtreeChecked(element, checked);
-			    }
-			}*/
 		});
 	}
 
