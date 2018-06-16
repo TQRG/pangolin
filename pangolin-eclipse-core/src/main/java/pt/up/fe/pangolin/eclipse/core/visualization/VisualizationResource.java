9b96b235420978701be355108a84c43769a15789
Alexandre Perez <alexandrecperez@gmail.com>
2018-06-16 11:20:05 +0100

    Fix UI plugin reference.

--- pangolin-eclipse-core/src/main/java/pt/up/fe/pangolin/eclipse/core/visualization/VisualizationResource.java
+++ pangolin-eclipse-core/src/main/java/pt/up/fe/pangolin/eclipse/core/visualization/VisualizationResource.java
@@ -10,7 +10,7 @@ public class VisualizationResource {
 
 	public static String get() {
 		try {
-			Bundle bundle = Platform.getBundle("pangolin-eclipse-ui");
+			Bundle bundle = Platform.getBundle("pt.up.fe.pangolin.eclipse.ui");
 			URL url = bundle.getEntry("resources/visualization/index.html");
 			return FileLocator.resolve(url).toString();
 		} catch (Exception e) {
